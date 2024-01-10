b02bf51205b1a3ab8e426f0e14cc5f4a389b4d5f
Lei Zhang <kinkikid2005@hotmail.com>
2024-01-10 21:57:27 +0800

    20240110

--- vm/create_vm_from_sig_img_ver.py
+++ vm/create_vm_from_sig_img_ver.py
@@ -4,15 +4,15 @@ from azure.identity import DefaultAzureCredential
 from azure.mgmt.network import NetworkManagementClient
 from azure.mgmt.compute import ComputeManagementClient
 
-location = "westeurope"
+location = "germanywestcentral"
 #sub_id_1 = "c69f7dec-22a1-4f72-a0b1-07811a7ed54b"
-sub_id_1 = "166157a8-9ce9-400b-91c7-1d42482b83d6"
-rg_name_1 = "nio-rg"
-vnet_name = "nio-vnet"
+sub_id = "166157a8-9ce9-400b-91c7-1d42482b83d6"
+rg_name = "sig-rg"
+vnet_name = "sig-vnet"
 snet_name = "subnet-1"
 nic_name = "lei-nic"
 vmss_name = "lei-vmss"
-vm_name = "lei-vm"
+vm_name = "lei-vm-new"
 vm_size = "Standard_B2als_v2"
 os_disk_name = vm_name + "-os-disk"
 os_disk_size_gb = 100
@@ -21,32 +21,31 @@ data_disk_name = vm_name + "-data-disk"
 data_disk_size_gb = 8192
 data_disk_sku = "Premium_LRS"
 
-sub_id_2 = "166157a8-9ce9-400b-91c7-1d42482b83d6"
-rg_name_2 = "nio-rg"
-sig_name = "gallery01"
-sig_img_name = "rocky8"
+
+sig_name = "nio_image_template_fk"
+sig_img_name = "lei-sig-img"
 sig_img_ver_name = "0.0.1"
 
 network_client = NetworkManagementClient(
     credential = DefaultAzureCredential(),
-    subscription_id = sub_id_1
+    subscription_id = sub_id
 )
 
 compute_client = ComputeManagementClient(
     credential = DefaultAzureCredential(),
-    subscription_id = sub_id_1
+    subscription_id = sub_id
 )
 
 start_time = time.time()
 nic = network_client.network_interfaces.begin_create_or_update(
-    resource_group_name = rg_name_1,
+    resource_group_name = rg_name,
     network_interface_name = nic_name,
     parameters = {
         "location": location,
         "ip_configurations": [{
             "name": "nic-ip-config",
             "subnet": {
-                "id": "/subscriptions/" + sub_id_1 + "/resourceGroups/" + rg_name_1 + "/providers/Microsoft.Network/virtualNetworks/" + vnet_name + "/subnets/" + snet_name
+                "id": "/subscriptions/" + sub_id + "/resourceGroups/" + rg_name + "/providers/Microsoft.Network/virtualNetworks/" + vnet_name + "/subnets/" + snet_name
             }
         }],
         "enable_accelerated_networking": True
@@ -57,15 +56,18 @@ print("Created NIC:\n{}".format(nic))
 
 start_time = time.time()
 vm = compute_client.virtual_machines.begin_create_or_update(
-    resource_group_name = rg_name_1,
+    resource_group_name = rg_name,
     vm_name = vm_name,
     parameters = {
         "location": location,
-        "plan": {
-            "name": "free",
-            "product": "rockylinux",
-            "publisher": "erockyenterprisesoftwarefoundationinc1653071250513"
-        },
+        #从get_gallery_image里获得的publisher,name, product，动态的填入下面的plan里
+        #如果是ubuntu环境，则把plan内容全部去掉
+        #如果是rocky环境，则不需要注释下面的环境，具体可以参考get_gallery_image.py
+        #"plan": {
+        #    "name": "free",
+        #    "product": "rockylinux",
+        #    "publisher": "erockyenterprisesoftwarefoundationinc1653071250513"
+        #},
         "zones": [
             "1"
         ],
@@ -74,7 +76,7 @@ vm = compute_client.virtual_machines.begin_create_or_update(
         },
         "storage_profile": {
             "image_reference": {
-                "id": "/subscriptions/" + sub_id_2 + "/resourceGroups/" + rg_name_2 + "/providers/Microsoft.Compute/galleries/" + sig_name + "/images/" + sig_img_name + "/versions/" + sig_img_ver_name
+                "id": "/subscriptions/" + sub_id + "/resourceGroups/" + rg_name + "/providers/Microsoft.Compute/galleries/" + sig_name + "/images/" + sig_img_name + "/versions/" + sig_img_ver_name
             },
             "os_disk": {
                 "os_type": "Linux",
@@ -102,7 +104,8 @@ vm = compute_client.virtual_machines.begin_create_or_update(
             #]
         },
         "additional_capabilities": {
-            "ultra_ssd_enabled": True
+            #"ultra_ssd_enabled": True
+            "ultra_ssd_enabled": False
         },
         "os_profile": {
             "computer_name": "test-computer.abc.com",
@@ -112,7 +115,7 @@ vm = compute_client.virtual_machines.begin_create_or_update(
         "network_profile": {
             "network_interfaces": [
                 {
-                    "id": "/subscriptions/" + sub_id_1 + "/resourceGroups/" + rg_name_1 + "/providers/Microsoft.Network/networkInterfaces/" + nic_name,
+                    "id": "/subscriptions/" + sub_id + "/resourceGroups/" + rg_name + "/providers/Microsoft.Network/networkInterfaces/" + nic_name,
                     "primary": True,
                     "delete_option": "Delete"
                 }
--- vm/get_gallery_image.py
+++ vm/get_gallery_image.py
@@ -4,42 +4,26 @@ from azure.identity import DefaultAzureCredential
 from azure.mgmt.network import NetworkManagementClient
 from azure.mgmt.compute import ComputeManagementClient
 
-location = "westeurope"
+location = "germanywestcentral"
 #sub_id_1 = "c69f7dec-22a1-4f72-a0b1-07811a7ed54b"
-sub_id_1 = "166157a8-9ce9-400b-91c7-1d42482b83d6"
-rg_name_1 = "nio-rg"
-vnet_name = "nio-vnet"
-snet_name = "subnet-1"
-nic_name = "lei-nic"
-vmss_name = "lei-vmss"
-vm_name = "lei-vm"
-vm_size = "Standard_B2als_v2"
-os_disk_name = vm_name + "-os-disk"
-os_disk_size_gb = 100
-os_disk_sku = "Premium_LRS"
-data_disk_name = vm_name + "-data-disk"
-data_disk_size_gb = 8192
-data_disk_sku = "Premium_LRS"
+sub_id = "166157a8-9ce9-400b-91c7-1d42482b83d6"
+rg_name= "sig-rg"
+sig_name = "nio_image_template_fk"
 
-sub_id_2 = "166157a8-9ce9-400b-91c7-1d42482b83d6"
-rg_name_2 = "nio-rg"
-sig_name = "gallery01"
-sig_img_name = "rocky8"
-sig_img_ver_name = "0.0.1"
 
 network_client = NetworkManagementClient(
     credential = DefaultAzureCredential(),
-    subscription_id = sub_id_1
+    subscription_id = sub_id
 )
 
 compute_client = ComputeManagementClient(
     credential = DefaultAzureCredential(),
-    subscription_id = sub_id_1
+    subscription_id = sub_id
 )
 
 
 start_time = time.time()
-images = compute_client.gallery_images.list_by_gallery(rg_name_1,sig_name)
+images = compute_client.gallery_images.list_by_gallery(rg_name,sig_name)
 for image in images:
     print(image.purchase_plan.publisher)
     print(image.purchase_plan.name)
