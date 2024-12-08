30bb193b7575bd194f7cf9a0739554a9fc04d6f7
Lei Zhang (AZURE ARCHITECT) <leizha@microsoft.com>
2024-12-08 18:00:29 +0800

    2024-12-08

--- azuremanagement/mysqlflexible/create_mysqlflexibleserver_privateendpoint/1.create_privatednszone_mysql.py
+++ azuremanagement/mysqlflexible/create_mysqlflexibleserver_privateendpoint/1.create_privatednszone_mysql.py
@@ -4,16 +4,21 @@ from azure.identity import DefaultAzureCredential,ClientSecretCredential
 from azure.mgmt.privatedns import PrivateDnsManagementClient
 from azure.mgmt.network import NetworkManagementClient
 
+'''
+这里的代码只需要在创建private dns zone的时候，执行1次就可以了
+'''
+
 def main():
     tenantid = os.environ.get('nonprod_tenantid')
     clientid = os.environ.get('nonprod_clientid')
     clientsecret = os.environ.get('nonprod_clientsecret')
 
+
     #这里要设置订阅名称,是MySQL RDS新建的Private DNS Zone
-    pe_dnszone_subscription_id = "166157a8-9ce9-400b-91c7-1d42482b83d6"
+    pe_dnszone_subscription_id = "c4959ac6-4963-4b67-90dd-da46865b607f"
 
     #这里要设置订阅名称，是DD订阅下的，已经有的Private DNS Zone
-    dd_dnszone_subscription_id = "166157a8-9ce9-400b-91c7-1d42482b83d6"
+    dd_dnszone_subscription_id = "074b8f7e-9eb5-4c38-b5f9-a39cf7876bdb"
     
     # Create client
     clientcredential = ClientSecretCredential(tenantid,clientid,clientsecret)
@@ -30,29 +35,33 @@ def main():
 
  
     #DD订阅，资源组名称
-    dd_rgname = "sig-rg"
+    dd_rgname = "defaultrg"
     #DD订阅，要创建的Private DNSZone名称
     dd_privatednzone_name = "privatelink.mysql.database.azure.com"  
 
     #PE订阅，资源组名称
-    pe_rgname = "sig-rg"
+    pe_rgname = "defaultrg"
     #PE要建立Virtual Network的VPC名称
-    pe_virtualnetwork_name = "NIO-PE-EU"
+    pe_virtualnetwork_name_1 = "NIO-PE-EU"
+    pe_virtualnetwork_name_2 = "NIO-PE-EU-OPS"
 
     #自定义标签
+   # 自定义标签
     custom_tags = {
-        'Environment': 'Development',
-        'Department': 'IT'
+                    "Environment": "stg",
+                    "Department": "POW",
+                    "ServiceInstance": "pe-db-stg"
     }
-
-    #首先在DD订阅下，新建Private DNS Zone
+    ###########################
+    #1.首先在DD订阅下，新建Private DNS Zone
     response = dd_privatedns_management_client.private_zones.begin_create_or_update(
         dd_rgname, dd_privatednzone_name, parameters={"location": "Global", "tags": custom_tags},
     ).result()
 
     print(response)
 
-    #新建Private DNS Zone后，还要与DD已有的OPS VPC建立Link关系
+    ###########################
+    #2.新建Private DNS Zone后，还要与DD已有的OPS VPC建立Link关系
     #DD订阅的资源组名称
     #DD订阅，要建立Virtual Network的VPC名称
     dd_virtualnetwork_name = "NIO-EU-OPS"
@@ -80,7 +89,60 @@ def main():
         },
     ).result()
 
+    ###########
+    #3.新建Private DNS Zone后，还要与PE EU link
 
+    pe_network_client = NetworkManagementClient(
+        credential = clientcredential,
+        subscription_id = pe_dnszone_subscription_id
+    )
+
+    pe_virtualnetwork = pe_network_client.virtual_networks.get(pe_rgname,pe_virtualnetwork_name_1)
+
+    dd_privatedns_management_client.virtual_network_links.begin_create_or_update(
+        resource_group_name = pe_rgname,
+        private_zone_name = dd_privatednzone_name,
+        virtual_network_link_name = "link-pe-eu",
+        parameters={
+            "location": "Global",
+            "properties": {
+                "registrationEnabled": False,
+                "virtualNetwork": {
+                    "id": pe_virtualnetwork.id
+                },
+            },
+            "tags": custom_tags,
+        },
+    ).result()
+
+
+
+    ###########
+    #3.新建Private DNS Zone后，还要与PE EU OPS link
+
+
+    pe_network_client = NetworkManagementClient(
+        credential = clientcredential,
+        subscription_id = pe_dnszone_subscription_id
+    )
+
+    pe_virtualnetwork = pe_network_client.virtual_networks.get(pe_rgname,pe_virtualnetwork_name_2)
+
+    dd_privatedns_management_client.virtual_network_links.begin_create_or_update(
+        resource_group_name = pe_rgname,
+        private_zone_name = dd_privatednzone_name,
+        virtual_network_link_name = "link-pe-eu-ops",
+        parameters={
+            "location": "Global",
+            "properties": {
+                "registrationEnabled": False,
+                "virtualNetwork": {
+                    "id": pe_virtualnetwork.id
+                },
+            },
+            "tags": custom_tags,
+        },
+    ).result()
 
 if __name__ == "__main__":
     main()
--- azuremanagement/mysqlflexible/create_mysqlflexibleserver_privateendpoint/5.create_azuremysql_privateendpoint.py
+++ azuremanagement/mysqlflexible/create_mysqlflexibleserver_privateendpoint/5.create_azuremysql_privateendpoint.py
@@ -1,5 +1,5 @@
 import os
-from azure.identity import DefaultAzureCredential,ClientSecretCredential
+from azure.identity import DefaultAzureCredential, ClientSecretCredential
 from azure.mgmt.rdbms.mysql_flexibleservers import MySQLManagementClient
 from azure.mgmt.network import NetworkManagementClient
 from azure.mgmt.privatedns import PrivateDnsManagementClient
@@ -11,67 +11,70 @@ from azure.mgmt.resource import ResourceManagementClient
 
 '''
 
+
 def main():
     tenantid = os.environ.get('nonprod_tenantid')
     clientid = os.environ.get('nonprod_clientid')
     clientsecret = os.environ.get('nonprod_clientsecret')
 
-    #这里要设置PE订阅ID
+    # 这里要设置PE订阅ID
     pe_subscription_id = "c4959ac6-4963-4b67-90dd-da46865b607f"
 
-    #PE的资源组名称
+    # PE的资源组名称
     pe_rg_name = "defaultrg"
 
-    #DD的订阅ID
+    # DD的订阅ID
     dd_subscription_id = "074b8f7e-9eb5-4c38-b5f9-a39cf7876bdb"
-    #DD的资源组名称
+    # DD的资源组名称
     dd_rg_name = "defaultrg"
-    
-    #设置数据中心区域
+
+    # 设置数据中心区域
     location = "germanywestcentral"
 
-    #之前创建好的MySQL名称
-    mysql_name = "leizhangproduction-00"
-    #新建的链接名称
+    # 之前创建好的MySQL名称
+    mysql_name = "s-azure-fk-pe-apollo-01"
+    # 新建的链接名称
     pe_pvt_endpoint_name = mysql_name + "-pvtendpoint"
-    #新建链接的时候，会创建1个网卡，设置网卡的名称
-    pe_pvt_endpont_nic_name =  pe_pvt_endpoint_name +"-nic"
+    # 新建链接的时候，会创建1个网卡，设置网卡的名称
+    pe_pvt_endpont_nic_name = pe_pvt_endpoint_name + "-nic"
 
-    #之前创建好的Virtual Network Name
+    # 之前创建好的Virtual Network Name
     pe_vnet_name = "NIO-PE-EU"
 
-    #之前创建好的subnet name
+    # 之前创建好的subnet name
     pe_subnet_name = "STG-EU-AZURE-PE-BE-MYSQL-01"
 
-    #自定义标签
+    # 自定义标签
     custom_tags = {
-        'Environment': 'Development',
-        'Department': 'IT'
-    }
+                    "Environment": "stg",
+                    "Department": "POW",
+                    "ServiceInstance": "pe-db-stg"
+                }
 
-    #实例化对象
-    clientcredential = ClientSecretCredential(tenantid,clientid,clientsecret)
+    # 实例化对象
+    clientcredential = ClientSecretCredential(tenantid, clientid, clientsecret)
 
-    #实例化对象
-    mysql_client = MySQLManagementClient(credential=clientcredential,subscription_id=pe_subscription_id)
-    pe_network_client =  NetworkManagementClient(credential = clientcredential, subscription_id = pe_subscription_id)
+    # 实例化对象
+    mysql_client = MySQLManagementClient(credential=clientcredential, subscription_id=pe_subscription_id)
+    pe_network_client = NetworkManagementClient(credential=clientcredential, subscription_id=pe_subscription_id)
 
-    #Private DNS Zone创建在DD订阅里
-    dd_privatedns_management_client = PrivateDnsManagementClient(credential = clientcredential, subscription_id = dd_subscription_id)
+    # Private DNS Zone创建在DD订阅里
+    dd_privatedns_management_client = PrivateDnsManagementClient(credential=clientcredential,
+                                                                 subscription_id=dd_subscription_id)
 
-    #1.先获得MySQL的资源ID
-    mysql = mysql_client.servers.get(pe_rg_name,mysql_name)
+    # 1.先获得MySQL的资源ID
+    mysql = mysql_client.servers.get(pe_rg_name, mysql_name)
     mysql_id = mysql.id
 
-    #2.再获得MySQL链接到的Virtual Network的信息，还有子网的信息
-    subnet = pe_network_client.subnets.get(pe_rg_name,pe_vnet_name,pe_subnet_name)
+    # 2.再获得MySQL链接到的Virtual Network的信息，还有子网的信息
+    subnet = pe_network_client.subnets.get(pe_rg_name, pe_vnet_name, pe_subnet_name)
     subnet_id = subnet.id
 
-    #这里写死 mysqlServer
+    # 这里写死 mysqlServer
     mysqlserver = "mysqlServer"
     group_ids = list(mysqlserver.split(" "))
 
-    #3.创建Private Link Endpoint
+    # 3.创建Private Link Endpoint
     response = pe_network_client.private_endpoints.begin_create_or_update(
         pe_rg_name,
         pe_pvt_endpoint_name,
@@ -79,43 +82,42 @@ def main():
             "location": location,
             "custom_network_interface_name": pe_pvt_endpont_nic_name,
             "private_link_service_connections": [
-            {
-              "name": pe_pvt_endpoint_name,
-              #需要链接的MySQL资源ID
-              "private_link_service_id": mysql_id,
-              #Group Ids这里用 mysqlServer
-              "group_ids": group_ids
+                {
+                    "name": pe_pvt_endpoint_name,
+                    # 需要链接的MySQL资源ID
+                    "private_link_service_id": mysql_id,
+                    # Group Ids这里用 mysqlServer
+                    "group_ids": group_ids
+                }
+            ],
+            "subnet": {
+                "id": subnet_id
             }
-          ],
-          "subnet": {
-            "id": subnet_id
-          }
         }
     ).result()
 
     print(response)
-    
-    #4. 先拿到Private Endpoint里面已经设置好的内网IP地址
-    pvt_endpoint = pe_network_client.private_endpoints.get(pe_rg_name,pe_pvt_endpoint_name)
+
+    # 4. 先拿到Private Endpoint里面已经设置好的内网IP地址
+    pvt_endpoint = pe_network_client.private_endpoints.get(pe_rg_name, pe_pvt_endpoint_name)
     pvt_endpoint_privateip = pvt_endpoint.custom_dns_configs[0].ip_addresses[0]
     pvt_endpoint_privateip_list = list(pvt_endpoint_privateip.split(" "))
 
-    
-    #5.上面设置完毕后，还需要更新private dns zone group
-    #指向到之前创建的Private DNS Zone
-    #https://learn.microsoft.com/en-us/rest/api/virtualnetwork/private-dns-zone-groups/create-or-update?view=rest-virtualnetwork-2024-01-01&tabs=HTTP
+    # 5.上面设置完毕后，还需要更新private dns zone group
+    # 指向到之前创建的Private DNS Zone
+    # https://learn.microsoft.com/en-us/rest/api/virtualnetwork/private-dns-zone-groups/create-or-update?view=rest-virtualnetwork-2024-01-01&tabs=HTTP
 
-    #先找到private dns zone所在的资源组名称
-    #有可能和MySQL不在一个资源组里
+    # 先找到private dns zone所在的资源组名称
+    # 有可能和MySQL不在一个资源组里
 
-    #不要修改下面的值
+    # 不要修改下面的值
     dd_privatednszone_name = "privatelink.mysql.database.azure.com"
 
-    #设置group name
+    # 设置group name
     privatednszone_group_name = "privatelink-mysql-database-azure-com"
     privatednszone_group_fqdn_name = mysql_name + "." + dd_privatednszone_name
 
-    dd_private_dnszone = dd_privatedns_management_client.private_zones.get(dd_rg_name,dd_privatednszone_name)
+    dd_private_dnszone = dd_privatedns_management_client.private_zones.get(dd_rg_name, dd_privatednszone_name)
     dd_private_dnszone_id = dd_private_dnszone.id
 
     # https://learn.microsoft.com/en-us/python/api/azure-mgmt-network/azure.mgmt.network.operations.privatednszonegroupsoperations?view=azure-python
@@ -123,20 +125,20 @@ def main():
     response = pe_network_client.private_dns_zone_groups.begin_create_or_update(
         dd_rg_name,
         pe_pvt_endpoint_name,
-        #设置name 为 default
-        private_dns_zone_group_name = "default",
+        # 设置name 为 default
+        private_dns_zone_group_name="default",
         parameters={
-           # "name": "default",
+            # "name": "default",
             "properties": {
                 "privateDnsZoneConfigs": [
                     {
                         "name": privatednszone_group_name,
-                        "properties":{
+                        "properties": {
                             "privateDnsZoneId": dd_private_dnszone_id,
-                            "record_sets":[
+                            "record_sets": [
                                 {
                                     "record_type": "A",
-                                    "record_set_name":mysql_name,
+                                    "record_set_name": mysql_name,
                                     "fqdn": privatednszone_group_fqdn_name,
                                     "ttl": 10,
                                     "ip_addresses": pvt_endpoint_privateip_list
