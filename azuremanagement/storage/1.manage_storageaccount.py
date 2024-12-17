ab80cc1129cf608214862959e64d1817d16de493
Lei Zhang (AZURE ARCHITECT) <leizha@microsoft.com>
2024-12-17 12:42:08 +0800

    2024-12-17

--- azuremanagement/storage/1.manage_storageaccount.py
+++ azuremanagement/storage/1.manage_storageaccount.py
@@ -4,47 +4,86 @@ import os
 from azure.identity import ClientSecretCredential, DefaultAzureCredential
 from azure.mgmt.storage import StorageManagementClient
 from azure.mgmt.resource import ResourceManagementClient
+from azure.core.exceptions import ResourceNotFoundError
 
-
+'''
+创建存储账户
+'''
 def main():
     tenantid = os.environ.get('nonprod_tenantid')
     clientid = os.environ.get('nonprod_clientid')
     clientsecret = os.environ.get('nonprod_clientsecret')
 
-    SUBSCRIPTION_ID = "166157a8-9ce9-400b-91c7-1d42482b83d6"
-    GROUP_NAME = "test-rg"
-    STORAGE_ACCOUNT = "leiteststorage001"
+    #订阅名称
+    subscription_id = "166157a8-9ce9-400b-91c7-1d42482b83d6"
+    #资源组名称
+    resourcegroup_name = "test-rg"
+    #存储账户名称
+    storage_account_name = "leiteststorage666"
+    #Azure存储账户所在数据中心区域，germanywestcentral就是Azure德国法兰克福
     location = "germanywestcentral"
 
+    #自定义标签
+    custom_tags = {
+        'Environment': 'Development',
+        'Department': 'IT'
+    }
     # Create client
     clientcredential = ClientSecretCredential(tenantid,clientid,clientsecret)
 
     # # For other authentication approaches, please see: https://pypi.org/project/azure-identity/
     resource_client = ResourceManagementClient(
         credential=clientcredential,
-        subscription_id=SUBSCRIPTION_ID
+        subscription_id=subscription_id
     )
     storage_client = StorageManagementClient(
         credential=clientcredential,
-        subscription_id=SUBSCRIPTION_ID
+        subscription_id=subscription_id
     )
+    try:
+      # Get storage account
+      storage_account = storage_client.storage_accounts.get_properties(
+            resourcegroup_name,
+            storage_account_name
+      )
+    except ResourceNotFoundError:
+      #没找到该资源,则创建该资源
+      storage_account = storage_client.storage_accounts.begin_create(
+          resourcegroup_name,
+          storage_account_name,
+          {
+            "sku": {
+              "name": "Standard_LRS"
+            },
+            "kind": "StorageV2",
+            "location": location,
+            "encryption": {
+              "services": {
+                "file": {
+                  "key_type": "Account",
+                  "enabled": True
+                },
+                "blob": {
+                  "key_type": "Account",
+                  "enabled": True
+                }
+              },
+              "key_source": "Microsoft.Storage"
+            },
+            "tags": custom_tags
+          }
+      ).result()
+      print("Create storage account:\n{}".format(storage_account))
 
-    # Create resource group
-    # resource_client.resource_groups.create_or_update(
-    #     GROUP_NAME,
-    #     {"location": "eastus"}
-    # )
-
-    # Create storage account
-    storage_account = storage_client.storage_accounts.begin_create(
-        GROUP_NAME,
-        STORAGE_ACCOUNT,
+    
+    #Update storage account
+    storage_account = storage_client.storage_accounts.update(
+        resourcegroup_name,
+        storage_account_name,
         {
-          "sku": {
-            "name": "Standard_LRS"
+          "network_acls": {
+            "default_action": "Allow"
           },
-          "kind": "StorageV2",
-          "location": location,
           "encryption": {
             "services": {
               "file": {
@@ -58,45 +97,10 @@ def main():
             },
             "key_source": "Microsoft.Storage"
           },
-          "tags": {
-            "key1": "value1",
-            "key2": "value2"
-          }
+          "tags": custom_tags
         }
-    ).result()
-    print("Create storage account:\n{}".format(storage_account))
-
-    # Get storage account
-    storage_account = storage_client.storage_accounts.get_properties(
-        GROUP_NAME,
-        STORAGE_ACCOUNT
     )
-    print("Get storage account:\n{}".format(storage_account))
-
-    # Update storage account
-    # storage_account = storage_client.storage_accounts.update(
-    #     GROUP_NAME,
-    #     STORAGE_ACCOUNT,
-    #     {
-    #       "network_acls": {
-    #         "default_action": "Allow"
-    #       },
-    #       "encryption": {
-    #         "services": {
-    #           "file": {
-    #             "key_type": "Account",
-    #             "enabled": True
-    #           },
-    #           "blob": {
-    #             "key_type": "Account",
-    #             "enabled": True
-    #           }
-    #         },
-    #         "key_source": "Microsoft.Storage"
-    #       }
-    #     }
-    # )
-    # print("Update storage account:\n{}".format(storage_account))
+    print("Update storage account:\n{}".format(storage_account))
     
     # Delete storage account
     # storage_account = storage_client.storage_accounts.delete(
--- azuremanagement/storage/2.manage_blob_container.py
+++ azuremanagement/storage/2.manage_blob_container.py
@@ -1,123 +1,109 @@
 
-import os
+import os,sys
 
-from azure.identity import DefaultAzureCredential, ClientSecretCredential
+from azure.identity import ClientSecretCredential, DefaultAzureCredential
 from azure.mgmt.storage import StorageManagementClient
 from azure.mgmt.resource import ResourceManagementClient
+from azure.core.exceptions import ResourceNotFoundError
 
-tenantid = os.environ.get('tenantid')
-clientid = os.environ.get('clientid')
-clientsecret = os.environ.get('clientsecret')
+'''
+创建Container
+'''
 
 def main():
+    tenantid = os.environ.get('nonprod_tenantid')
+    clientid = os.environ.get('nonprod_clientid')
+    clientsecret = os.environ.get('nonprod_clientsecret')
 
-    SUBSCRIPTION_ID = "b5aa1700-1510-4f35-b134-fe9c7c695df1"
-    GROUP_NAME = "test-rg"
-    STORAGE_ACCOUNT = "leizhangstorage00"
+    #订阅名称
+    subscription_id = "166157a8-9ce9-400b-91c7-1d42482b83d6"
+    #资源组名称
+    resourcegroup_name = "test-rg"
+    #存储账户名称
+    storage_account_name = "leiteststorage666"
+    #Azure存储账户所在数据中心区域，germanywestcentral就是Azure德国法兰克福
     location = "germanywestcentral"
-    blobcontainer_name = "privatecontainer"
-
+    #container名称
+    blobcontainer_name = "privatecontainer000"
+    
+    #自定义标签
+    custom_tags = {
+        'Environment': 'Development',
+        'Department': 'IT'
+    }
     # Create client
     clientcredential = ClientSecretCredential(tenantid,clientid,clientsecret)
 
     # # For other authentication approaches, please see: https://pypi.org/project/azure-identity/
     resource_client = ResourceManagementClient(
         credential=clientcredential,
-        subscription_id=SUBSCRIPTION_ID
+        subscription_id=subscription_id
     )
     storage_client = StorageManagementClient(
         credential=clientcredential,
-        subscription_id=SUBSCRIPTION_ID
+        subscription_id=subscription_id
     )
-
-    # Create resource group
-    # resource_client.resource_groups.create_or_update(
-    #     GROUP_NAME,
-    #     {"location": "eastus"}
-    # )
-
-    # Create storage account
-    '''
-    storage_account = storage_client.storage_accounts.begin_create(
-        GROUP_NAME,
-        STORAGE_ACCOUNT,
-        {
-          "sku": {
-            "name": "Standard_LRS"
-          },
-          "kind": "StorageV2",
-          "location": location,
-          "allow_blob_public_access": False,
-          "encryption": {
-            "services": {
-              "file": {
-                "key_type": "Account",
-                "enabled": True
-              },
-              "blob": {
-                "key_type": "Account",
-                "enabled": True
-              }
-            },
-            "key_source": "Microsoft.Storage"
-          },
-          "tags": {
-            "key1": "value1",
-            "key2": "value2"
-          }
-        }
-    ).result()
-    print("Create storage account:\n{}".format(storage_account))
-
- 
-   # Create blob container
-    blob_container = storage_client.blob_containers.create(
-        GROUP_NAME,
-        STORAGE_ACCOUNT,
-        blobcontainer_name,
-        {}
-    )
-    print("Create blob container:\n{}".format(blob_container))
-    '''
+    try:
+      # Get storage account
+      storage_account = storage_client.storage_accounts.get_properties(
+            resourcegroup_name,
+            storage_account_name
+      )
+    except ResourceNotFoundError:
+      sys.exit(0)
     
     # Get blob container
-    blob_container = storage_client.blob_containers.get(
-        GROUP_NAME,
-        STORAGE_ACCOUNT,
-        blobcontainer_name
-    )
-    print("Get blob container:\n{}".format(blob_container))
-    
+    try:
+      blob_container = storage_client.blob_containers.get(resourcegroup_name,storage_account_name,blobcontainer_name)
+    except ResourceNotFoundError:
+      print("Container not found. Try to create a new container\n")
+      
+      # Create blob container
+      blob_container = storage_client.blob_containers.create(resourcegroup_name,storage_account_name,blobcontainer_name,{})
+      print("Create blob container:\n{}".format(blob_container))
 
-    # Allow Blob anonymous access = Enable
-    storage_client.storage_accounts.update(
-        GROUP_NAME,STORAGE_ACCOUNT,
-        {
-          "allow_blob_public_access": True,
-        }
-    )
-    print("Update storage account:\n{}".format(storage_account))
+      # Get blob container
+      blob_container = storage_client.blob_containers.get(
+          resourcegroup_name,
+          storage_account_name,
+          blobcontainer_name
+      )
+      print("Get blob container:\n{}".format(blob_container))
+      
+
+      ###################################################################
+      # 在存储账户级别，打开匿名访问功能
+      # 请谨慎操作
+      ###################################################################
+      # storage_client.storage_accounts.update(
+      #     resourcegroup_name,storage_account_name,
+      #     {
+      #       "allow_blob_public_access": True,
+      #     }
+      # )
+      # print("Update storage account:\n{}".format(storage_account))
 
 
-    ###################################################################
-    ###################################################################
-    ###################################################################
+      ###################################################################
+      # 把Container类型设置为Public，允许匿名访问
+      # 请谨慎操作
+      ###################################################################
 
       # Update blob container
-    blob_container = storage_client.blob_containers.update(
-        GROUP_NAME,
-        STORAGE_ACCOUNT,
-        blobcontainer_name,
-        {
-            
-          "public_access": "Blob",
-          "metadata": {
-            "metadata": "true"
-          }
-        }
-    )
-    print("Update blob container:\n{}".format(blob_container))
-
+      # blob_container = storage_client.blob_containers.update(
+      #     resourcegroup_name,
+      #     storage_account_name,
+      #     blobcontainer_name,
+      #     {
+              
+      #       "public_access": "Blob",
+      #       "metadata": {
+      #         "metadata": "true"
+      #       }
+      #     }
+      # )
+      # print("Update blob container:\n{}".format(blob_container))
+       
 
 if __name__ == "__main__":
     main()
