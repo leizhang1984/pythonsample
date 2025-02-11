bd2501ae9e9a8d30f8fcab907395adc530f32e06
Lei Zhang (AZURE ARCHITECT) <leizha@microsoft.com>
2025-02-11 10:55:55 +0800

    Update 1.create_support.py

--- azuremanagement/support/1.create_support.py
+++ azuremanagement/support/1.create_support.py
@@ -1,6 +1,7 @@
 
 import os
 import time
+import datetime
 
 from azure.identity import ClientSecretCredential, DefaultAzureCredential
 from azure.mgmt.support import MicrosoftSupport
@@ -8,6 +9,109 @@ from azure.mgmt.support.models import ContactProfile, SupportTicketDetails, Serv
 
 '''
 创建支持工单
+
+这里主要有2个内容：
+1)需要在Case里，拿到Service
+    List Service:
+    https://learn.microsoft.com/en-us/rest/api/support/services/list?view=rest-support-2024-04-01&tabs=HTTP
+
+      "id": "/providers/Microsoft.Support/services/cddd3eb5-1830-b494-44fd-782f691479dc",
+      "name": "cddd3eb5-1830-b494-44fd-782f691479dc",
+      "type": "Microsoft.Support/services",
+      "properties": {
+        "displayName": "Virtual Machine running Linux",
+        "resourceTypes": [
+          "MICROSOFT.CLASSICCOMPUTE/VIRTUALMACHINES",
+          "MICROSOFT.COMPUTE/VIRTUALMACHINES"
+        ]
+      }
+
+2) 需要显示Problem Classification
+https://learn.microsoft.com/en-us/rest/api/support/problem-classifications/list?view=rest-support-2024-04-01&tabs=HTTP
+
+serviceName = cddd3eb5-1830-b494-44fd-782f691479dc
+
+{
+  "value": [
+    {
+      "id": "/providers/Microsoft.Support/services/cddd3eb5-1830-b494-44fd-782f691479dc/problemClassifications/bae68529-51c1-d9b0-9e84-157fe59ac631",
+      "name": "bae68529-51c1-d9b0-9e84-157fe59ac631",
+      "type": "Microsoft.Support/problemClassifications",
+      "properties": {
+        "displayName": "Compute-VM (cores-vCPUs) subscription limit increases"
+      }
+    },
+    {
+      "id": "/providers/Microsoft.Support/services/cddd3eb5-1830-b494-44fd-782f691479dc/problemClassifications/7d8fb79f-7fbc-7125-b4dc-3060a75a755d",
+      "name": "7d8fb79f-7fbc-7125-b4dc-3060a75a755d",
+      "type": "Microsoft.Support/problemClassifications",
+      "properties": {
+        "displayName": "Cannot connect to my VM / My configuration change impacted connectivity",
+        "secondaryConsentEnabled": [
+          {
+            "description": "Azure Support may need to access your virtual machine's memory to diagnose the problem. Support may pause it for up to 10 minutes.",
+            "type": "VirtualMachineMemoryDump"
+          }
+        ]
+      }
+    },
+    {
+      "id": "/providers/Microsoft.Support/services/cddd3eb5-1830-b494-44fd-782f691479dc/problemClassifications/b3ca1e6d-ea88-4d89-2f27-cf22e3bf6b37",
+      "name": "b3ca1e6d-ea88-4d89-2f27-cf22e3bf6b37",
+      "type": "Microsoft.Support/problemClassifications",
+      "properties": {
+        "displayName": "Cannot connect to my VM / Troubleshoot my network security group (NSG)",
+        "secondaryConsentEnabled": [
+          {
+            "description": "Azure Support may need to access your virtual machine's memory to diagnose the problem. Support may pause it for up to 10 minutes.",
+            "type": "VirtualMachineMemoryDump"
+          }
+        ]
+      }
+    },
+    {
+      "id": "/providers/Microsoft.Support/services/cddd3eb5-1830-b494-44fd-782f691479dc/problemClassifications/64f558d7-19ec-b533-1d51-bf8065b8b000",
+      "name": "64f558d7-19ec-b533-1d51-bf8065b8b000",
+      "type": "Microsoft.Support/problemClassifications",
+      "properties": {
+        "displayName": "Cannot connect to my VM / Troubleshoot my VM firewall",
+        "secondaryConsentEnabled": [
+          {
+            "description": "Azure Support may need to access your virtual machine's memory to diagnose the problem. Support may pause it for up to 10 minutes.",
+            "type": "VirtualMachineMemoryDump"
+          }
+        ]
+      }
+    },
+    {
+      "id": "/providers/Microsoft.Support/services/cddd3eb5-1830-b494-44fd-782f691479dc/problemClassifications/55947c35-0935-27d8-69fa-6fb7798312ab",
+      "name": "55947c35-0935-27d8-69fa-6fb7798312ab",
+      "type": "Microsoft.Support/problemClassifications",
+      "properties": {
+        "displayName": "Cannot connect to my VM / I have an issue with my public IP",
+        "secondaryConsentEnabled": [
+          {
+            "description": "Azure Support may need to access your virtual machine's memory to diagnose the problem. Support may pause it for up to 10 minutes.",
+            "type": "VirtualMachineMemoryDump"
+          }
+        ]
+      }
+    },
+    {
+      "id": "/providers/Microsoft.Support/services/cddd3eb5-1830-b494-44fd-782f691479dc/problemClassifications/aeca6c5b-eeda-205d-8e9a-84fbc3787d58",
+      "name": "aeca6c5b-eeda-205d-8e9a-84fbc3787d58",
+      "type": "Microsoft.Support/problemClassifications",
+      "properties": {
+        "displayName": "Cannot connect to my VM / I need to reset my password",
+        "secondaryConsentEnabled": [
+          {
+            "description": "Azure Support may need to access your virtual machine's memory to diagnose the problem. Support may pause it for up to 10 minutes.",
+            "type": "VirtualMachineMemoryDump"
+          }
+        ]
+      }
+    },
+
 '''
 def main():
     tenantid = os.environ.get('nonprod_tenantid')
@@ -70,7 +174,11 @@ def main():
     )
 
     #注意Ticket Name名字要唯一
-    ticket_name = "sample_support_ticket_0"
+    # 格式化当前日期和时间为字符串
+    now = datetime.datetime.now()
+    current_datetime_str = now.strftime("%Y_%m_%d_%H_%M_%S")
+
+    ticket_name = "VM_support_ticket_" + current_datetime_str
 
     #创建支持工单,
     support_ticket = support_client.support_tickets.begin_create(
@@ -85,19 +193,7 @@ if __name__ == "__main__":
     main()
 
 '''
-    List Service:
-    https://learn.microsoft.com/en-us/rest/api/support/services/list?view=rest-support-2024-04-01&tabs=HTTP
 
-      "id": "/providers/Microsoft.Support/services/cddd3eb5-1830-b494-44fd-782f691479dc",
-      "name": "cddd3eb5-1830-b494-44fd-782f691479dc",
-      "type": "Microsoft.Support/services",
-      "properties": {
-        "displayName": "Virtual Machine running Linux",
-        "resourceTypes": [
-          "MICROSOFT.CLASSICCOMPUTE/VIRTUALMACHINES",
-          "MICROSOFT.COMPUTE/VIRTUALMACHINES"
-        ]
-      }
 '''
 
 '''
