6644b98f7d58c250e1aecc95f66c4872a34a82ec
Lei Zhang (AZURE ARCHITECT) <leizha@microsoft.com>
2025-05-10 23:23:26 +0800

    Update 2.query_support_bymail.py

--- azuremanagement/support/2.query_support_bymail.py
+++ azuremanagement/support/2.query_support_bymail.py
@@ -1,4 +1,4 @@
-import os
+import os,pytz
 from azure.identity import ClientSecretCredential, DefaultAzureCredential
 from azure.mgmt.support import MicrosoftSupport
 from azure.mgmt.resource import SubscriptionClient
@@ -13,6 +13,9 @@ def main():
 
     #需要查询的邮箱地址
     search_email_address = "leizha@microsoft.com"
+    
+    #显示北京时间
+    target_timezone = pytz.timezone('Asia/Shanghai')
 
     #订阅名称
     #subscription_id = "166157a8-9ce9-400b-91c7-1d42482b83d6"  
@@ -35,8 +38,11 @@ def main():
             #如果联系人邮箱相同
             if primary_email_address == search_email_address:
 
+                #默认是UTC时区，转换一下时区
+                ticket_created_date_chinatime = ticket.created_date.astimezone(target_timezone)
+
                 #工单的：标题，描述，紧急程度，状态
-                print(f"Ticket id: {ticket.support_ticket_id}, title: {ticket.title}, severity: {ticket.severity}, status: {ticket.status}")
+                print(f"Ticket id: {ticket.support_ticket_id}, create time: {ticket_created_date_chinatime.strftime("%Y-%m-%d %H:%M:%S")}, title: {ticket.title}, severity: {ticket.severity}, status: {ticket.status}")
                 #print(f"Ticket id: {ticket.support_ticket_id}, title: {ticket.title}, description: {ticket.description}, severity: {ticket.severity}, status: {ticket.status}")
 
                 
