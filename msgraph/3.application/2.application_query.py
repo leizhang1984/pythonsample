e655b178b852e673b37f95f87388b5cb77e62885
Lei Zhang (AZURE ARCHITECT) <leizha@microsoft.com>
2025-06-25 14:46:54 +0800

    Create 2.application_query.py

---
+++ msgraph/3.application/2.application_query.py
@@ -0,0 +1,34 @@
+import os
+import asyncio
+#pip3 install msgraph-sdk
+from azure.identity import ClientSecretCredential
+from msgraph import GraphServiceClient
+from msgraph.generated.applications.applications_request_builder import ApplicationsRequestBuilder
+
+
+async def main():
+    tenantid = os.environ.get('msdn_tenantid')
+    clientid = os.environ.get('msdn_clientid')
+    clientsecret = os.environ.get('msdn_clientsecret')
+
+    #Graph API
+    # Create a credential object. Used to authenticate requests
+    ### https://github.com/microsoftgraph/msgraph-sdk-python/blob/main/docs/users_samples.md
+    # Create a credential object. Used to authenticate requests
+    credential = ClientSecretCredential(tenantid,clientid,clientsecret)
+    scopes = ['https://graph.microsoft.com/.default']
+    graph_service_client = GraphServiceClient(credentials=credential, scopes=scopes)
+
+    #List All Application
+    '''
+    参考: https://learn.microsoft.com/en-us/graph/api/application-list?view=graph-rest-1.0&tabs=http
+    检查Permissions
+    '''
+
+    # Get by Application id (Using Application Object ID)
+    app  = await graph_service_client.applications.by_application_id("135d72ae-dc87-482a-87e6-a275892e16f9").get()
+    print(app.display_name, app.id)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
