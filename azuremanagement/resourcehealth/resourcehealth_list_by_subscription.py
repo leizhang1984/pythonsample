609af460df96a17fb8ca6bce865517f8f9dc5399
Lei Zhang (AZURE ARCHITECT) <leizha@microsoft.com>
2025-08-29 14:59:25 +0800

    2025-08-29

---
+++ azuremanagement/resourcehealth/resourcehealth_list_by_subscription.py
@@ -0,0 +1,39 @@
+import os
+from azure.identity import ClientSecretCredential
+from azure.mgmt.compute import ComputeManagementClient
+from azure.mgmt.resourcehealth import ResourceHealthMgmtClient
+#pip3  install  azure-mgmt-resourcehealth
+
+def get_vm_resource_health(subscription_id, tenant_id, client_id, client_secret, resource_group_name, vm_name):
+    # 使用ClientSecretCredential进行身份验证
+    credential = ClientSecretCredential(tenant_id, client_id, client_secret)
+
+    # 创建Resource Health管理客户端
+    resource_health_client = ResourceHealthMgmtClient(credential, subscription_id)
+
+    try:
+
+        response = resource_health_client.events.list_by_subscription_id()
+        for item in response:
+            print(f"Time is {item.impact_start_time}, Reason is {item.reason}, Title is {item.title}, summary is {item.summary}")
+
+    except Exception as e:
+        print(f"Error retrieving resource health for VM '{vm_name}': {e}")
+
+def main():
+     # 替换为你的Service Principal信息
+    tenant_id = os.environ.get('nonprod_tenantid')
+    client_id = os.environ.get('nonprod_clientid')
+    client_secret = os.environ.get('nonprod_clientsecret')
+
+    subscription_id = 'ffe7c7cd-f495-444a-9df7-9bcb39da2d66'
+    
+    # 指定的资源组名称和虚拟机名称
+    resource_group_name = 'RG-TEST'
+    vm_name = 'canary-vm'
+
+    get_vm_resource_health(subscription_id, tenant_id, client_id, client_secret, resource_group_name, vm_name)
+
+
+if __name__ == "__main__":
+    main()
