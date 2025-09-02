f688805433cf5c74975e02841e0a12cf35c4152e
Lei Zhang (AZURE ARCHITECT) <leizha@microsoft.com>
2025-09-02 20:38:32 +0800

    Update aiops.py

--- aiops/aiops.py
+++ aiops/aiops.py
@@ -174,7 +174,7 @@ def get_vm_monitor_metrics(tenant_id, client_id, client_secret,subscription_id, 
     aggregation = "Maximum"
 
     #metric_result = monitor_client.metrics.list(resource_id,timespan,"PT5M",metric_name,aggregation,None,None,None)
-    metric_result = monitor_client.metrics.list(resource_id,timespan,"PT5M",metric_name,aggregation,None,None,None)
+    metric_result = monitor_client.metrics.list(resource_id,timespan,"PT1M",metric_name,aggregation,None,None,None)
 
     # 设置时区信息
     utc = pytz.utc
