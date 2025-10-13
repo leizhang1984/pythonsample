cf25044975ac84e2a3691dafbfdba0f0701f93cb
Lei Zhang (AZURE ARCHITECT) <leizha@microsoft.com>
2025-10-13 11:47:24 +0800

    20251013

---
+++ azuremanagement/eventhub/sync-with-connectionstring/receive.py
@@ -0,0 +1,52 @@
+import os
+from azure.eventhub import EventHubConsumerClient
+
+# 替换以下变量为你的 Event Hub 的相关信息
+CONNECTION_STR = ""
+EVENTHUB_NAME = ""
+CONSUMER_GROUP = "01"
+
+def on_event(partition_context, event):
+    # Put your code here.
+    # If the operation is i/o intensive, multi-thread will have better performance.
+    print("Received event from partition: {}.".format(partition_context.partition_id))
+    print(f"Data: {event.body_as_str()}")
+    #partition_context.update_checkpoint(event)
+
+def on_partition_initialize(partition_context):
+    # Put your code here.
+    print("Partition: {} has been initialized.".format(partition_context.partition_id))
+
+def on_partition_close(partition_context, reason):
+    # Put your code here.
+    print("Partition: {} has been closed, reason for closing: {}.".format(partition_context.partition_id, reason))
+
+def on_error(partition_context, error):
+    # Put your code here. partition_context can be None in the on_error callback.
+    if partition_context:
+        print(
+            "An exception: {} occurred during receiving from Partition: {}.".format(
+                partition_context.partition_id, error
+            )
+        )
+    else:
+        print("An exception: {} occurred during the load balance process.".format(error))
+
+if __name__ == "__main__":
+    consumer_client = EventHubConsumerClient.from_connection_string(
+        conn_str=CONNECTION_STR,
+        consumer_group=CONSUMER_GROUP,
+        eventhub_name=EVENTHUB_NAME
+    )
+
+    try:
+        with consumer_client:
+            consumer_client.receive(
+                on_event=on_event,
+                on_partition_initialize=on_partition_initialize,
+                on_partition_close=on_partition_close,
+                on_error=on_error,
+                starting_position="-1",  # "-1" is from the beginning of the partition.
+            )
+    except KeyboardInterrupt:
+        print("Stopped receiving.")
